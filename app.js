bd1b512ef2643ef389edcfea57d0facf75f2933d
OmkarSatishHirve <93387887+OmkarSatishHirve@users.noreply.github.com>
2021-11-17 13:23:09 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+window.onload = () =>{
+    document.querySelector('#calculate').onclick = calculateTip;
+}
+function calculateTip(){
+    let amount = document.querySelector('#amount').value;
+    let persons = document.querySelector('#persons').value;
+    let service = document.querySelector('#services').value;
+
+        console.log(service);
+        if(amount === '' && service === 'Select'){
+            alert("please enter valid value");
+            return;
+        }
+
+         if(persons === '1'){
+              document.querySelector('#each').style.display = 'none';
+            }else{
+                document.querySelector('#each').style.display = 'block';
+    }
+
+    let total = (amount*service) / persons;
+    total=Math.round(total*100)/100;
+    total=total.toFixed(2);
+
+    document.querySelector('.tip').style.display = 'block';
+    document.querySelector('#total').innerHTML = total;
+}
